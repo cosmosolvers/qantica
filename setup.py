95a1732fc10b3992ec02f853f78cd60fddcd7056
Hydromel Victor Doledji <victorvaddely@gmail.com>
2024-03-30 13:25:24 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qantica',
+    version='1.0.0',
+    description='faire des api en flask en utilisant un model samblable a django',
+    author='Hydromel Victor Doledji',
+    author_email='victorvaddely@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Liste des dépendances si nécessaire
+    ],
+)
